6aefb9f0ae16e72158f478e931b028f5bc162f3b
Maddox <roman.kolisnyk2001@gmail.com>
2020-08-03 18:22:02 +0300

    Kolisnyk. Upgrade module QuickOrder

--- QuickOrder/view/frontend/web/js/components/send-info.js
+++ QuickOrder/view/frontend/web/js/components/send-info.js
@@ -43,6 +43,10 @@ define([
                 email = $("#customerEmail").val();
                 sku = $("#product_addtocart_form").attr("data-product-sku");
 
+                // if (typeof sku == "undefined") {
+                //     sku = $("#popup-modal-info").attr("data-product-sku");
+                // } else {
+
                 if (
                     typeof (name) == "string" && name.length != 0 &&
                     Number.isInteger(phoneIsNumber) == true && phoneIsNumber != 0 && phone.length != 0 &&
@@ -67,6 +71,8 @@ define([
                         console.log(response);
                         $("#errorMessage").hide();
                         $("#quick-order-form").modal("closeModal");
+                        console.log(sku);
+                        console.log(typeof (sku));
                     }).fail(function (error) {
                         console.log(JSON.stringify(error));
                     });
@@ -75,9 +81,10 @@ define([
                         "Your data is not valid. Please, check the correct spelling</span>");
                 }
 
+                //}
+
                 function correctEmail(email) {
                     let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                    //^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
                     return reg.test(email);
                 }
             });
